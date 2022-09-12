de7d86b774e2f6fbcbc811166aeb268ea69ae728
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-09-12 13:53:03 +0100

    wip: mock basic header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import GlobalStyle from './App.styles';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <p>Hello</p>
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function Header() {
-  return <p>Header</p>;
+  return (
+    <header>
+      <div>
+        <div>
+          <a href="/">HOME</a>
+          <a href="/">ABOUT</a>
+          <a href="/">CONTACT</a>
+        </div>
+        <div>
+          <a href="/">Facebook</a>
+          <a href="/">Twitter</a>
+          <a href="/">Github</a>
+        </div>
+      </div>
+      <div>
+        <img src="./src/images/logo.png" alt="uk runner logo" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
