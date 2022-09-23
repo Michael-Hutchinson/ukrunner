1d77e95d883d4e9711eaece70c9a89cc023d4f23
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-09-23 15:56:29 +0100

    Feature - Add strava fetch for activities
    - add activities component to the page
    - update env names
    - add error handling if access token is expired

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,7 @@
 import { ActivityData } from './strava';
 
-const clientID = import.meta.env.CLIENTID;
-const clientSecret = import.meta.env.CLIENTSECRET;
+const clientID = import.meta.env.VITE_CLIENTID;
+const clientSecret = import.meta.env.VITE_CLIENTSECRET;
 const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`;
 
 export const getActivities = ({
@@ -15,16 +15,15 @@ export const getActivities = ({
 }) => {
   fetch(`${callActivities}${accessToken}`)
     .then((res) => res.json())
-    .then(
-      (data) => {
+    .then((data) => {
+      if (data.message === 'Authorization Error') {
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        getAccessToken({ setLoading, setActivities });
+      } else {
         setActivities(data);
         setLoading(false);
-      },
-      (error) => {
-        // eslint-disable-next-line no-console
-        console.log(error);
-      },
-    );
+      }
+    });
 };
 
 export const getAccessToken = ({
@@ -34,15 +33,18 @@ export const getAccessToken = ({
   setLoading: (state: boolean) => void;
   setActivities: (state: ActivityData[]) => void;
 }) => {
-  const refreshToken = import.meta.env.REFRESHTOKEN;
+  const refreshToken = import.meta.env.VITE_REFRESHTOKEN;
   const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
   const currentAccessToken = localStorage.getItem('accessToken');
-  if (!currentAccessToken) {
+  if (!currentAccessToken || currentAccessToken === 'undefined') {
     fetch(callRefresh, {
       method: 'POST',
     })
       .then((res) => res.json())
-      .then((result) => localStorage.setItem('accessToken', result.access_token));
+      .then((result) => {
+        localStorage.setItem('accessToken', result.access_token);
+        getActivities({ accessToken: result.access_token, setLoading, setActivities });
+      });
   } else {
     getActivities({ accessToken: currentAccessToken, setLoading, setActivities });
   }
