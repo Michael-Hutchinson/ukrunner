a98ac1d24b60205e0d6e9c7ed46da17be60e9875
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-09-27 18:54:39 +0100

    feat: import navbar to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import GlobalStyle from './App.styles';
 import Activities from './components/Activities/Activities';
 import Header from './components/Header/Header';
+import Navbar from './components/shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Header />
+      <Navbar />
       <Activities />
     </div>
   );
