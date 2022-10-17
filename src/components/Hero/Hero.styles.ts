989a7d8eb570e5053e43991f75a45b3043e1ec2b
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-17 17:15:18 +0100

    feat: add hero captions
    - add headings and subheadings to image hovers

--- src/components/Hero/Hero.styles.ts
+++ src/components/Hero/Hero.styles.ts
@@ -27,17 +27,48 @@ export const NavLink = styled(Link)`
   &#shop {
     grid-area: shop;
   }
+  position: relative;
 `;
 
 export const Image = styled.img`
   width: 100%;
+  height: 100%;
 `;
 
 export const ImageBox = styled.div`
   display: grid;
-  grid-template-columns: repeat(1fr, 4);
-  grid-template-rows: 1fr 1fr;
   grid-template-areas: 'news news about about' 'race events training shop';
   padding: 20px 0 20px;
   width: 85%;
+`;
+
+export const Caption = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 170, 0, 0.5);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  :hover {
+    opacity: 1;
+  }
+`;
+
+export const Heading = styled.h3`
+  color: var(--white);
+  margin: 0;
+  text-transform: uppercase;
+  font-size: 0.875rem;
+`;
+
+export const SubHeading = styled.p`
+  color: var(--white);
+  margin: 0;
+  font-size: 1.125rem;
+  text-align: center;
 `;
