f3f4c50e8bde0c402b7914a4ddba3dda5a807ea9
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-19 21:08:48 +0100

    feat: create footer component
    - add footer sections
    - add responsive styles

---
+++ src/components/PageWrapper/Footer/Footer.styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const PageFooter = styled.footer`
+  background-color: #111;
+  text-align: center;
+  border-top: 8px solid var(--yellow);
+  padding: 3.75rem 2.5rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  @media (max-width: 48rem) {
+    grid-template-columns: auto;
+  }
+`;
+
+export const Bottom = styled.section`
+  grid-column-start: 1;
+  grid-column-end: 4;
+  display: flex;
+  justify-content: space-evenly;
+  @media (max-width: 48rem) {
+    grid-column-end: auto;
+    flex-direction: column;
+    p:first-child {
+      margin-bottom: 0;
+    }
+  }
+  p {
+    color: var(--white);
+  }
+}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  font-size: 0.875rem;
+`;
