025daf9c39c32b60895ef08ca27f166a5e50a7f7
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-19 11:41:00 +0100

    feat: wrap the provider around the main component tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import StravaProvider from './helpers/context';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
-root.render(<App />);
+root.render(
+  <StravaProvider>
+    <App />
+  </StravaProvider>,
+);
