679edfd86a5981966b1bc58381b47bceff82d813
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-19 16:31:43 +0100

    fix: create constant page titles to dynamically change

---
+++ src/constants/PageTitles.ts
@@ -0,0 +1,15 @@
+const defaultTitle = '| UK Runner';
+
+const PageTitles = {
+  Home: `Homepage ${defaultTitle}`,
+  About: `About me ${defaultTitle}`,
+  Blog: `Latest news ${defaultTitle}`,
+  Race: `Race History ${defaultTitle}`,
+  Training: `Training schedule ${defaultTitle}`,
+  Events: `Events ${defaultTitle}`,
+  Shop: `Shop ${defaultTitle}`,
+  Contact: `Contact ${defaultTitle}`,
+  Error: `Page not found ${defaultTitle}`,
+};
+
+export default PageTitles;
