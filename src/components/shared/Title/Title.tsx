7b23b803914e743d40eafdfac7054757059aec15
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-20 16:12:51 +0100

    fix: update title to pass either H1 or H2 depending on use

--- src/components/shared/Title/Title.tsx
+++ src/components/shared/Title/Title.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import { Section, SubHeading } from './Title.styles';
 
 interface ITitleProps {
-  h2Text: string;
+  h1Text?: string;
+  h2Text?: string;
   smallText: string;
 }
 
-function Title({ h2Text, smallText }: ITitleProps) {
+function Title({ h1Text, h2Text, smallText }: ITitleProps) {
   return (
     <Section>
-      <h2>{h2Text}</h2>
+      {h1Text && <h1>{h1Text}</h1>}
+      {h2Text && <h2>{h2Text}</h2>}
       <SubHeading>{smallText}</SubHeading>
     </Section>
   );
 }
 
 export default Title;
+
+Title.defaultProps = {
+  h1Text: '',
+  h2Text: '',
+};
