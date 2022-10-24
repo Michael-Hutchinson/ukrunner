051af9d61fb3a24a2600f5d4e6c26d5fb059c45f
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-24 14:57:48 +0100

    fix: remove placeholder code for viewing blog posts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import { collection } from 'firebase/firestore';
 import PageWrapper from '../../components/PageWrapper/PageWrapper/PageWrapper';
 import PageTitles from '../../constants/PageTitles';
 import About from './About/About';
 import Hero from './Hero/Hero';
-import { db } from '../../helpers/firebase';
 
 function Home() {
-  const [value, loading, error] = useCollection(collection(db, 'blog'), {
-    snapshotListenOptions: { includeMetadataChanges: true },
-  });
   return (
     <PageWrapper title={PageTitles.Home}>
       <>
         <Hero />
         <About />
-        <p>
-          {error && <strong>Error: {JSON.stringify(error)}</strong>}
-          {loading && <span>Loading...</span>}
-          {value && (
-            <>
-              {value.docs.map((doc) => (
-                <>
-                  <p key={doc.id}>{doc.data().title}</p>
-                  <p>{doc.data().body}</p>
-                </>
-              ))}
-            </>
-          )}
-        </p>
       </>
     </PageWrapper>
   );
