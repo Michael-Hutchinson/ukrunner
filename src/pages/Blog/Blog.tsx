9ccec62d907e52cb5f79bb5dc2792a55de293991
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-24 14:59:03 +0100

    feat: map through blog posts and display on page

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection } from 'firebase/firestore';
 import PageWrapper from '../../components/PageWrapper/PageWrapper/PageWrapper';
 import PageTitles from '../../constants/PageTitles';
+import { db } from '../../helpers/firebase';
 
 function Blog() {
+  const [value, loading, error] = useCollection(collection(db, 'blog'));
   return (
     <PageWrapper title={PageTitles.Blog}>
-      <h1>Blog</h1>
+      <>
+        <h1>Blog</h1>
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Loading...</span>}
+        {value && (
+          <>
+            {value.docs.map((post) => (
+              <div key={post.id}>
+                <p>{post.data().title}</p>
+                <p>{post.data().body}</p>
+              </div>
+            ))}
+          </>
+        )}
+      </>
     </PageWrapper>
   );
 }
