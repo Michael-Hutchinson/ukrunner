af9cbb11295742cc2409509cd7dd0f33c1c4046f
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-24 14:54:21 +0100

    feat: add link to view list of blog posts

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,5 +1,6 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PageWrapper from '../../components/PageWrapper/PageWrapper/PageWrapper';
 import Button from '../../components/shared/Button/Button';
 import PageTitles from '../../constants/PageTitles';
@@ -11,6 +12,13 @@ function Admin() {
       <>
         <h1>Admin</h1>
         <Button onClick={() => signOut(auth)} buttonType="button" buttonText="Logout" />
+        <ul>
+          <li>
+            <Link style={{ color: 'red' }} to="/admin/blog">
+              view blogs
+            </Link>
+          </li>
+        </ul>
       </>
     </PageWrapper>
   );
