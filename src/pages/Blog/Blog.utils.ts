3d6991b0a6dd6c73a3ebc0532cf5d4ae7f65baf9
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-25 15:32:44 +0100

    feat: create function to get all blog posts

--- src/pages/Blog/Blog.utils.ts
+++ src/pages/Blog/Blog.utils.ts
@@ -1,6 +1,7 @@
 import { doc, setDoc, Timestamp, collection, getDocs, deleteDoc, getDoc } from 'firebase/firestore';
 import { NavigateFunction } from 'react-router-dom';
 import { db } from '../../helpers/firebase';
+import { IBlog } from '../../types/Blog.types';
 
 export const saveBlog = (title: string, body: string, navigate: NavigateFunction, message: string) => {
   const splitTitle = title.replaceAll(' ', '-');
@@ -31,7 +32,7 @@ export const editBlog = (title: string, body: string, navigate: NavigateFunction
 
 export const getBlog = (
   blogID: string,
-  setBlog: (blog: { title: string; body: string; date: Date }) => void,
+  setBlog: (blog: IBlog) => void,
   setOriginalTitle: (originalTitle: string) => void,
 ) => {
   const docRef = doc(db, 'blog', blogID);
@@ -46,5 +47,21 @@ export const getBlog = (
       setBlog(blog);
       setOriginalTitle(blogData?.title);
     }
+  });
+};
+
+export const getBlogs = (setBlogs: (blog: IBlog[]) => void) => {
+  getDocs(collection(db, 'blog')).then((response) => {
+    const blogs: IBlog[] = [];
+    response.forEach((blog) => {
+      const singleBlog = blog.data();
+      const blogData = {
+        title: singleBlog?.title,
+        body: singleBlog?.body,
+        date: singleBlog?.date,
+      };
+      blogs.push(blogData);
+    });
+    setBlogs(blogs);
   });
 };
