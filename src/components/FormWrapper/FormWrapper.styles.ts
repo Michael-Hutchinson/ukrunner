c155545fd26957f0db348bf5a4aab0afadd9e749
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-26 13:38:09 +0100

    feat: create form wrapper component

---
+++ src/components/FormWrapper/FormWrapper.styles.ts
@@ -0,0 +1,31 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+export const FormContainer = styled(Box)`
+  margin-bottom: 20px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  -webkit-box-shadow: 0 1px 1px rgb(0 0 0 / 5%);
+  box-shadow: 0 1px 1px rgb(0 0 0 / 5%);
+`;
+
+export const FormHeader = styled(Box)`
+  background-color: #f5f5f5;
+  border-bottom: 1px solid #ddd;
+  padding: 15px;
+  display: flex;
+  svg {
+    padding-right: 10px;
+  }
+`;
+
+export const FormBody = styled(Box)`
+  padding: 0 15px;
+`;
+
+export const FormFooter = styled(Box)`
+  padding: 10px 0;
+  border-top: 1px solid #ddd;
+  background-color: #f5f5f5;
+`;
