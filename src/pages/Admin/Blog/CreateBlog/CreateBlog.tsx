34d6c10ba818c54fc2a36906286b30bb6155d333
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-26 13:36:26 +0100

    fix: add button type & admin wrapper

--- src/pages/Admin/Blog/CreateBlog/CreateBlog.tsx
+++ src/pages/Admin/Blog/CreateBlog/CreateBlog.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Alert } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import PageWrapper from '../../../../components/PageWrapper/PageWrapper';
-import Button from '../../../../components/shared/Button/Button';
-import Title from '../../../../components/shared/Title/Title';
+import Button, { ButtonTypes } from '../../../../components/shared/Button/Button';
 import PageTitles from '../../../../constants/PageTitles';
 import { getBlogTitles, saveBlog } from '../../../Blog/Blog.utils';
 import ReactQuillEditor from '../../../../components/QuillEditor/QuillEditor';
+import AdminWrapper from '../../../../components/AdminWrapper/AdminWrapper';
 
 function CreateBlog() {
   const [title, setTitle] = useState('');
@@ -18,35 +17,32 @@ function CreateBlog() {
   }, []);
   const navigate = useNavigate();
   return (
-    <PageWrapper title={PageTitles.Admin}>
-      <>
-        <Title h1Text="Add New Blog Posts Here" smallText="Here you can add new blog posts" />
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            saveBlog(title, body, navigate, 'blog created');
+    <AdminWrapper title={PageTitles.Admin} h1Text="Add New Blog Posts Here" smallText="Here you can add new blog posts">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveBlog(title, body, navigate, 'blog created');
+        }}
+      >
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            if (titles.includes(e.target.value.toLowerCase())) {
+              setValidation(true);
+            } else {
+              setValidation(false);
+            }
           }}
-        >
-          <input
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-              if (titles.includes(e.target.value.toLowerCase())) {
-                setValidation(true);
-              } else {
-                setValidation(false);
-              }
-            }}
-            type="text"
-            name="title"
-            required
-          />
-          <ReactQuillEditor value={body} onChange={setBody} />
-          <Button buttonType="submit" buttonText="Create Blog" disabled={validation} />
-          {validation && <Alert severity="error">Title already exists!</Alert>}
-        </form>
-      </>
-    </PageWrapper>
+          type="text"
+          name="title"
+          required
+        />
+        <ReactQuillEditor value={body} onChange={setBody} />
+        <Button buttonType={ButtonTypes.submit} buttonText="Create Blog" disabled={validation} />
+        {validation && <Alert severity="error">Title already exists!</Alert>}
+      </form>
+    </AdminWrapper>
   );
 }
 
