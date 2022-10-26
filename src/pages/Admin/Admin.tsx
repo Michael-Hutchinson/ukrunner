16d861a33251e8e4e6e92df6b6c329357e479e91
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-26 09:52:46 +0100

    feat: update admin wrapper to pass in title props

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import PageWrapper from '../../components/PageWrapper/PageWrapper';
-import Title from '../../components/shared/Title/Title';
 import PageTitles from '../../constants/PageTitles';
 import AdminWrapper from '../../components/AdminWrapper/AdminWrapper';
 
 function Admin() {
   return (
-    <PageWrapper title={PageTitles.Admin}>
+    <AdminWrapper
+      title={PageTitles.Admin}
+      h1Text="Admin Control Panel"
+      smallText="This is the admin control panel all website tasks can be completed here"
+    >
       <>
-        <Title
-          h1Text="Admin Control Panel"
-          smallText="This is the admin control panel all website tasks can be completed here"
-        />
-        <AdminWrapper>
-          <>
-            <h2>Welcome to the admin panel</h2>
-            <h3>Please use the navigation on the left hand side for the functions you require</h3>
-          </>
-        </AdminWrapper>
+        <h2>Welcome to the admin panel</h2>
+        <h3>Please use the navigation on the left hand side for the functions you require</h3>
       </>
-    </PageWrapper>
+    </AdminWrapper>
   );
 }
 
