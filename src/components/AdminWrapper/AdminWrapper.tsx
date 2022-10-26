7c124ed2301572f17c1dbfcb6ce0d4a832ca7e62
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-26 09:55:16 +0100

    feat: update admin wrapper to pass in title props (#41)

--- src/components/AdminWrapper/AdminWrapper.tsx
+++ src/components/AdminWrapper/AdminWrapper.tsx
@@ -1,23 +1,33 @@
 import { Container, Grid } from '@mui/material';
 import React, { ReactElement } from 'react';
+import PageWrapper from '../PageWrapper/PageWrapper';
+import Title from '../shared/Title/Title';
 import Sidebar from './Sidebar/Sidebar';
 
 interface IAdminWrapperProps {
   children: ReactElement;
+  h1Text: string;
+  smallText: string;
+  title: string;
 }
 
-function AdminWrapper({ children }: IAdminWrapperProps): ReactElement {
+function AdminWrapper({ children, h1Text, smallText, title }: IAdminWrapperProps): ReactElement {
   return (
-    <Container maxWidth="xl">
-      <Grid container spacing={2}>
-        <Grid item lg={3} md={4} xs={12}>
-          <Sidebar />
-        </Grid>
-        <Grid item lg={9} md={8} xs={12}>
-          <section>{children}</section>
-        </Grid>
-      </Grid>
-    </Container>
+    <PageWrapper title={title}>
+      <>
+        <Title h1Text={h1Text} smallText={smallText} />
+        <Container maxWidth="xl">
+          <Grid container spacing={2}>
+            <Grid item lg={3} md={4} xs={12}>
+              <Sidebar />
+            </Grid>
+            <Grid item lg={9} md={8} xs={12}>
+              <section>{children}</section>
+            </Grid>
+          </Grid>
+        </Container>
+      </>
+    </PageWrapper>
   );
 }
 
