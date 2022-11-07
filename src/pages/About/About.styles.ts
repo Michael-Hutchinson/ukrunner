5969466a17d09bd660c7a7d263138006ff9e5012
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-07 16:14:56 +0000

    feat: create about page placeholder text

---
+++ src/pages/About/About.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  width: 100%;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+`;
+
+export const Section = styled.section`
+  border-bottom: 1px solid var(--lightGrey);
+  padding-bottom: 4.563rem;
+`;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,45 @@
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import React from 'react';
 
 import PageWrapper from '../../components/PageWrapper/PageWrapper';
+import Title from '../../components/shared/Title/Title';
 import PageTitles from '../../constants/PageTitles';
+import { Img, Section } from './About.styles';
 
 function About() {
   return (
     <PageWrapper title={PageTitles.About}>
-      <h1>About</h1>
+      <>
+        <Title h1Text="About me" smallText="Find out everything about me here" />
+        <Section>
+          <Container>
+            <Grid container spacing={2}>
+              <Grid item xs={7}>
+                <h2>Hello everyone! I&apos;m Michael, an incredibly average runner based in Manchester, UK.</h2>
+                <p>
+                  If you want to know more just email me at michael@ukrunner.com or use the contact form found on the
+                  contact page.
+                </p>
+                <p>
+                  If you want to know more just email me at michael@ukrunner.com or use the contact form found on the
+                  contact page.
+                </p>
+                <p>
+                  If you want to know more just email me at michael@ukrunner.com or use the contact form found on the
+                  contact page.
+                </p>
+              </Grid>
+              <Grid item xs={5}>
+                <Img alt="" src="./src/images/about.png" />
+              </Grid>
+            </Grid>
+          </Container>
+        </Section>
+        <section>
+          <p>hello</p>
+        </section>
+      </>
     </PageWrapper>
   );
 }
