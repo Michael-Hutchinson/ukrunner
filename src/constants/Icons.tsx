ced8963babe141c4483e42bdf6c966c11c3f384f
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-07 22:37:14 +0000

    feat: add 2 new icons

--- src/constants/Icons.tsx
+++ src/constants/Icons.tsx
@@ -1,4 +1,6 @@
 import DeleteIcon from '@mui/icons-material/Delete';
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
 import EditIcon from '@mui/icons-material/Edit';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import PersonIcon from '@mui/icons-material/Person';
@@ -10,6 +12,8 @@ const Icons = {
   Edit: <EditIcon fontSize="small" />,
   Delete: <DeleteIcon fontSize="small" />,
   View: <VisibilityIcon fontSize="small" />,
+  Run: <DirectionsRunIcon fontSize="small" />,
+  Bike: <DirectionsBikeIcon fontSize="small" />,
 };
 
 export default Icons;
