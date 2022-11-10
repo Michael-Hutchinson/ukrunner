dee57301ea0ed3a3f695a4fbf07997aeb497a916
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-10 15:42:42 +0000

    feat: create styles for bg image and close button

--- src/pages/Admin/Blog/CreateBlog/CreateBlog.styles.ts
+++ src/pages/Admin/Blog/CreateBlog/CreateBlog.styles.ts
@@ -1,8 +1,26 @@
+import Fab from '@mui/material/Fab';
+import { styled as MUIStyled } from '@mui/material/styles';
 import styled from 'styled-components';
 
-const FileInput = styled.input`
+export const FileInput = styled.input`
   margin-top: 8px;
   margin-bottom: 16px;
 `;
 
-export default FileInput;
+export const BGImage = styled.div<{ background: string }>`
+  background-image: url('${({ background }) => background}');
+  padding-bottom: 40%;
+  width: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  position: relative;
+}
+`;
+
+export const CloseButton = MUIStyled(Fab)`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+`;
