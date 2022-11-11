f722f2ded0842041597436c3ee6884cef4874600
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-11 12:16:27 +0000

    feat: pass in user information

--- src/utils/Users.utils.ts
+++ src/utils/Users.utils.ts
@@ -2,13 +2,33 @@ import { doc, getDoc } from 'firebase/firestore';
 
 import { db } from '../helpers/firebase';
 
-const getUser = (userID: string) => {
+const getUser = (
+  userID: string,
+  setFirstName?: (firstName: string) => void,
+  setLastName?: (lastName: string) => void,
+  setProfilePicture?: (profilePicture: string) => void,
+  setBio?: (bio: string) => void,
+  setFileName?: (fileName: string) => void,
+) => {
   const docRef = doc(db, 'users', userID);
   getDoc(docRef).then((response) => {
     if (response.data()) {
       const userData = response.data();
-      // TODO - add users information here
-      console.log(userData);
+      if (setFirstName) {
+        setFirstName(userData?.firstName);
+      }
+      if (setLastName) {
+        setLastName(userData?.lastName);
+      }
+      if (setProfilePicture) {
+        setProfilePicture(userData?.profilePicture);
+      }
+      if (setBio) {
+        setBio(userData?.bio);
+      }
+      if (setFileName) {
+        setFileName(userData?.fileName);
+      }
     }
   });
 };
