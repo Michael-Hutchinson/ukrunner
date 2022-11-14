d2b362c45dcb20d441d9fcfe0b914dec25c65c6e
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-14 14:29:32 +0000

    feat: create edit user model

--- src/utils/Users.utils.ts
+++ src/utils/Users.utils.ts
@@ -1,8 +1,10 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
-import { db } from '../helpers/firebase';
+import { db, storage } from '../helpers/firebase';
+import { EditUser, User } from '../types/Users.types';
 
-const getUser = (
+export const getUser = (
   userID: string,
   setFirstName?: (firstName: string) => void,
   setSurname?: (surname: string) => void,
@@ -33,4 +35,51 @@ const getUser = (
   });
 };
 
-export default getUser;
+export const editUser = ({
+  userID,
+  firstName,
+  surname,
+  bio,
+  file,
+  fileName,
+  originalProfilePicture,
+  originalFileName,
+  setSuccess,
+  setSuccessMessage,
+}: EditUser) => {
+  let userData: User = {
+    firstName,
+    surname,
+    bio,
+  };
+  if (file && fileName) {
+    const userProfileRef = ref(storage, fileName);
+    uploadBytes(userProfileRef, file).then(() => {
+      getDownloadURL(userProfileRef).then((imageURL) => {
+        userData = {
+          ...userData,
+          fileName,
+          profilePicture: imageURL,
+        };
+        if (originalFileName && originalProfilePicture) {
+          const deleteRef = ref(storage, originalFileName);
+          deleteObject(deleteRef);
+        }
+        setDoc(doc(db, 'users', userID), userData).then(() => {
+          setSuccess(true);
+          setSuccessMessage('User profile edited');
+        });
+      });
+    });
+  } else {
+    userData = {
+      ...userData,
+      fileName: originalFileName,
+      profilePicture: originalProfilePicture,
+    };
+    setDoc(doc(db, 'users', userID), userData).then(() => {
+      setSuccess(true);
+      setSuccessMessage('User profile edited');
+    });
+  }
+};
