37d49c013f83a03ba14795a61b9e5f84ed9849c6
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-14 16:40:09 +0000

    feat: mobile navbar styling

--- src/components/AdminWrapper/Sidebar/Sidebar.styles.ts
+++ src/components/AdminWrapper/Sidebar/Sidebar.styles.ts
@@ -5,6 +5,20 @@ const Side = styled(Paper)`
   background-color: #fcf8e3;
   width: 320px;
   max-width: 100%;
+  @media (max-width: 48rem) {
+    width: 100%;
+    ul {
+      display: flex;
+      justify-content: space-evenly;
+      li,
+      a {
+        div {
+          display: flex;
+          justify-content: center;
+        }
+      }
+    }
+  }
 `;
 
 export default Side;
