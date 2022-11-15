4f01b19f45fca3d2d02044764077c88ad2c0f3f1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-15 16:13:39 +0000

    Bug - fix image imports (#124)
    * fix: update carousel images
    
    * fix: update about image import
    
    * fix: footer, header image import
    
    * fix: update a tags to link
    
    * fix: image grid image imports

--- src/pages/Home/Hero/ImageGrid/ImageGrid.tsx
+++ src/pages/Home/Hero/ImageGrid/ImageGrid.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import About from '../../../../images/homepage/about.png';
+import Events from '../../../../images/homepage/events.png';
+import News from '../../../../images/homepage/news.png';
+import Race from '../../../../images/homepage/race.png';
+import Shop from '../../../../images/homepage/shop.png';
+import Training from '../../../../images/homepage/training.png';
 import { Caption, Heading, Image, ImageBox, NavLink, Section, SubHeading } from './ImageGrid.styles';
 
 function ImageGrid() {
@@ -7,42 +13,42 @@ function ImageGrid() {
     <Section>
       <ImageBox>
         <NavLink id="news" to="/blog">
-          <Image alt="" src="./src/images/homepage/news.png" />
+          <Image alt="" src={News} />
           <Caption>
             <Heading>Latest News</Heading>
             <SubHeading>Keep up to date with my blog</SubHeading>
           </Caption>
         </NavLink>
         <NavLink id="about" to="/about">
-          <Image alt="" src="./src/images/homepage/about.png" />
+          <Image alt="" src={About} />
           <Caption>
             <Heading>About Me</Heading>
             <SubHeading>Find out about UKRunner.com</SubHeading>
           </Caption>
         </NavLink>
         <NavLink id="race" to="/race">
-          <Image alt="" src="./src/images/homepage/race.png" />
+          <Image alt="" src={Race} />
           <Caption>
             <Heading>Race History</Heading>
             <SubHeading>View all my recent race history and timed results</SubHeading>
           </Caption>
         </NavLink>
         <NavLink id="events" to="/events">
-          <Image alt="" src="./src/images/homepage/events.png" />
+          <Image alt="" src={Events} />
           <Caption>
             <Heading>Events</Heading>
             <SubHeading>View all events that I am currently signed up for</SubHeading>
           </Caption>
         </NavLink>
         <NavLink id="training" to="/training">
-          <Image alt="" src="./src/images/homepage/training.png" />
+          <Image alt="" src={Training} />
           <Caption>
             <Heading>Training Schedule</Heading>
             <SubHeading>View my weekly training schedule</SubHeading>
           </Caption>
         </NavLink>
         <NavLink id="shop" to="/shop">
-          <Image alt="" src="./src/images/homepage/shop.png" />
+          <Image alt="" src={Shop} />
           <Caption>
             <Heading>Shop</Heading>
             <SubHeading>View merchandise that I currently have on sale</SubHeading>
