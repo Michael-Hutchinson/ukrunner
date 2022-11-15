bae8d9d7fcd82ec61ea1523add8207a94afb6f26
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-15 15:52:22 +0000

    fix: update carousel images

--- src/pages/Home/Hero/Carousel/Carousel.tsx
+++ src/pages/Home/Hero/Carousel/Carousel.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom';
 
+import About from '../../../../images/homepage/about.png';
+import Events from '../../../../images/homepage/events.png';
+import News from '../../../../images/homepage/news.png';
+import Race from '../../../../images/homepage/race.png';
+import Shop from '../../../../images/homepage/shop.png';
+import Training from '../../../../images/homepage/training.png';
 import Section from './Carousel.styles';
 
 function MobileCarousel() {
@@ -9,27 +15,27 @@ function MobileCarousel() {
     <Section>
       <Carousel showArrows showThumbs={false} showStatus={false} autoPlay stopOnHover infiniteLoop swipeable>
         <Link id="news" to="/blog">
-          <img alt="" src="./src/images/homepage/news.png" />
+          <img alt="" src={News} />
           <p className="legend">Latest News</p>
         </Link>
         <Link id="about" to="/about">
-          <img alt="" src="./src/images/homepage/about.png" />
+          <img alt="" src={About} />
           <p className="legend">About Me</p>
         </Link>
         <Link id="race" to="/race">
-          <img alt="" src="./src/images/homepage/race.png" />
+          <img alt="" src={Race} />
           <p className="legend">Race History</p>
         </Link>
         <Link id="events" to="/events">
-          <img alt="" src="./src/images/homepage/events.png" />
+          <img alt="" src={Events} />
           <p className="legend">Events</p>
         </Link>
         <Link id="training" to="/training">
-          <img alt="" src="./src/images/homepage/training.png" />
+          <img alt="" src={Training} />
           <p className="legend">Training Schedule</p>
         </Link>
         <Link id="shop" to="/Shop">
-          <img alt="" src="./src/images/homepage/Shop.png" />
+          <img alt="" src={Shop} />
           <p className="legend">Shop</p>
         </Link>
       </Carousel>
