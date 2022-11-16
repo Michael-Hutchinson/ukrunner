8757ea509462abf26afe65827bd1c7c47698f48a
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-16 17:47:34 +0000

    fix: remove background color

--- src/pages/Home/RecentActivities/RecentActivities.styles.ts
+++ src/pages/Home/RecentActivities/RecentActivities.styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   justify-content: space-evenly;
-  background: var(--grey);
   border-bottom: 1px solid var(--lightGrey);
   padding-top: 4.438rem;
   padding-bottom: 4.563rem;
