1e4a9fe0cfe128fbb223536da877f591e8bca8cb
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-18 11:28:10 +0000

    feat: add title component

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import PageWrapper from '../../components/PageWrapper/PageWrapper';
+import Title from '../../components/shared/Title/Title';
 import PageTitles from '../../constants/PageTitles';
 
 function Register() {
   return (
     <PageWrapper title={PageTitles.Register}>
-      <h1>Register</h1>
+      <Title h1Text="Register" smallText="Create a new account here" />
     </PageWrapper>
   );
 }
