d5da81e9210750bc30927a62376c2f25a967e2ae
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-23 16:36:37 +0000

    feat: add lock icon

--- src/constants/Icons.tsx
+++ src/constants/Icons.tsx
@@ -2,6 +2,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
 import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
 import EditIcon from '@mui/icons-material/Edit';
+import LockIcon from '@mui/icons-material/Lock';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import PersonIcon from '@mui/icons-material/Person';
 import VisibilityIcon from '@mui/icons-material/Visibility';
@@ -14,6 +15,7 @@ const Icons = {
   View: <VisibilityIcon fontSize="small" />,
   Run: <DirectionsRunIcon fontSize="small" />,
   Bike: <DirectionsBikeIcon fontSize="small" />,
+  Lock: <LockIcon fontSize="small" />,
 };
 
 export default Icons;
