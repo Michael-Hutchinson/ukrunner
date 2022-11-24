73da03f952169d31a728a07e0d319b6a57af595e
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-11-24 19:51:10 +0000

    feat: create basic modal

---
+++ src/pages/Profile/FollowModal/FollowModal.tsx
@@ -0,0 +1,44 @@
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+const style = {
+  position: 'absolute' as const,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+interface ModalProps {
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+function FollowModal({ isOpen, handleClose }: ModalProps) {
+  return (
+    <div>
+      <Modal
+        open={isOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default FollowModal;
