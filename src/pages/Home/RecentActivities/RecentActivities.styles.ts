c1c427a4aba28ac79a55ce09c231d598772651ca
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-12-15 21:40:40 +0000

    Feature - styling homepage activities (#143)
    * feat: create activity homepage view
    
    * feat: add bottom section for activity grid
    
    * feat: add moving time and sport type

--- src/pages/Home/RecentActivities/RecentActivities.styles.ts
+++ src/pages/Home/RecentActivities/RecentActivities.styles.ts
@@ -1,12 +1,83 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Section = styled.section`
-  justify-content: space-evenly;
   border-bottom: 1px solid var(--lightGrey);
   padding-top: 4.438rem;
   padding-bottom: 4.563rem;
   text-align: center;
-  display: flex;
 `;
 
-export default Section;
+export const ActivitySection = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-rows: auto;
+  @media (max-width: 48rem) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const ActivityItem = styled.div`
+  padding: 3rem 2rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ActivityInner = styled.div`
+  max-width: 400px;
+  width: 100%;
+  margin: 0 auto;
+}`;
+
+export const ActivityTitle = styled.h3`
+  color: var(--yellow);
+  text-transform: uppercase;
+  text-align: left;
+  margin: 0;
+  font-size: 1.5rem;
+`;
+
+export const ActivityImage = styled.svg`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 5%;
+  max-height: 80%;
+  max-width: 300px;
+  z-index: 1;
+  opacity: 0.1;
+`;
+
+export const ActivitySubTitle = styled.span<{ small?: boolean }>`
+  font-size: 3.2rem;
+  font-weight: 600;
+  ${(props) =>
+    props.small &&
+    css`
+      font-size: 1.6rem;
+      text-align: left;
+    `}
+  color: #454545;
+  sub {
+    vertical-align: baseline;
+    font-size: 60%;
+  }
+`;
+
+export const TopSection = styled.div<{ bottom?: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  ${(props) =>
+    props.bottom &&
+    css`
+      margin-bottom: 0;
+    `}
+`;
+
+export const ParText = styled.p`
+  display: flex;
+  align-items: center;
+  opacity: 0.6;
+  margin: 0;
+`;
--- src/pages/Home/RecentActivities/RecentActivities.tsx
+++ src/pages/Home/RecentActivities/RecentActivities.tsx
@@ -1,44 +1,121 @@
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import TimerIcon from '@mui/icons-material/Timer';
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import Button, { ButtonTypes } from '../../../components/shared/Button/Button';
+import Title from '../../../components/shared/Title/Title';
 import { StravaContext } from '../../../helpers/context';
-import Section from './RecentActivities.styles';
+import {
+  ActivityImage,
+  ActivityInner,
+  ActivityItem,
+  ActivitySection,
+  ActivitySubTitle,
+  ActivityTitle,
+  ParText,
+  Section,
+  TopSection,
+} from './RecentActivities.styles';
 import convertPolylineIntoRouteInfo from './RecentActivities.utils';
 
 function RecentActivities() {
   const activityData = useContext(StravaContext);
+  const navigate = useNavigate();
   return (
     <Section>
-      {activityData?.activities?.slice(0, 2).map((activity) => {
-        const polyline = activity.map.summary_polyline
-          ? convertPolylineIntoRouteInfo(activity.map.summary_polyline)
-          : null;
-        return (
-          <div key={activity.id}>
-            <p>Name: {activity.name}</p>
-            <p>Distance: {activity.distance}</p>
-            {activity.type === 'Hike' ? (
-              <p>Moving Time: {activity.moving_time}</p>
-            ) : (
-              <p>Time: {activity.elapsed_time}</p>
-            )}
-            <p>Avg. Heartrate: {activity.average_heartrate}</p>
-            {polyline && (
-              <svg
-                viewBox={`${polyline.minX} ${polyline.minY} ${polyline.maxX - polyline.minX} ${
-                  polyline.maxY - polyline.minY
-                }`}
-              >
-                <polyline
-                  points={polyline.points}
-                  fill="none"
-                  stroke="black"
-                  strokeWidth={polyline.points.length / 100}
-                />
-              </svg>
-            )}
-          </div>
-        );
-      })}
+      <Title h2Text="Activities" smallText="All my strava activities" />
+      <ActivitySection>
+        {activityData?.activities?.slice(0, 2).map((activity) => {
+          const polyline = activity.map.summary_polyline
+            ? convertPolylineIntoRouteInfo(activity.map.summary_polyline)
+            : null;
+          return (
+            <ActivityItem key={activity.id}>
+              <ActivityInner>
+                <ActivityTitle>{activity.name}</ActivityTitle>
+                <div>
+                  <TopSection>
+                    <ActivitySubTitle>
+                      {(activity.distance / 1000).toFixed(2)}
+                      <sub>km</sub>
+                      <ParText>
+                        <NearMeIcon fontSize="small" />
+                        Distance
+                      </ParText>
+                    </ActivitySubTitle>
+                    {activity.type === 'Hike' ? (
+                      <ActivitySubTitle>
+                        {(activity.moving_time / 60).toFixed(2).replace('.', ':')}
+                        <ParText>
+                          <AccessTimeIcon fontSize="small" />
+                          Moving Time
+                        </ParText>
+                      </ActivitySubTitle>
+                    ) : (
+                      <ActivitySubTitle>
+                        {activity.elapsed_time}
+                        <ParText>
+                          <AccessTimeIcon fontSize="small" />
+                          Time
+                        </ParText>
+                      </ActivitySubTitle>
+                    )}
+                  </TopSection>
+                  <TopSection bottom>
+                    <ActivitySubTitle small>
+                      {((activity.moving_time / 60 / activity.distance) * 1000).toFixed(2)}
+                      <sub>/km</sub>
+                      <ParText>
+                        <TimerIcon fontSize="small" />
+                        Avg. Pace
+                      </ParText>
+                    </ActivitySubTitle>
+                    <ActivitySubTitle small>
+                      {activity.average_heartrate}
+                      <sub>bpm</sub>
+                      <ParText>
+                        <FavoriteIcon fontSize="small" />
+                        Avg. Heartrate
+                      </ParText>
+                    </ActivitySubTitle>
+                    <ActivitySubTitle small>
+                      {activity.sport_type}
+                      <ParText>
+                        {activity.type === ('Hike' || 'Run') ? (
+                          <DirectionsRunIcon fontSize="small" />
+                        ) : (
+                          <DirectionsBikeIcon fontSize="small" />
+                        )}
+                        Activity Type
+                      </ParText>
+                    </ActivitySubTitle>
+                  </TopSection>
+                </div>
+                {polyline && (
+                  <ActivityImage
+                    viewBox={`${polyline.minX} ${polyline.minY} ${polyline.maxX - polyline.minX} ${
+                      polyline.maxY - polyline.minY
+                    }`}
+                  >
+                    <polyline
+                      points={polyline.points}
+                      fill="none"
+                      stroke="black"
+                      strokeWidth={polyline.points.length / 100}
+                    />
+                  </ActivityImage>
+                )}
+              </ActivityInner>
+            </ActivityItem>
+          );
+        })}
+      </ActivitySection>
+      <Button buttonType={ButtonTypes.button} buttonText="View all activities" onClick={() => navigate('/training')} />
     </Section>
   );
 }
