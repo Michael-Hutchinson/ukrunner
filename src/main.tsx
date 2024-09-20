578a26ac779c85e5cdb16dadfbbcc7d56fe86749
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-09-20 19:49:26 +0100

    refactor: Update main.tsx to use react-query for data fetching
    - Import QueryClient and QueryClientProvider from react-query
    - Create a new QueryClient instance
    - Wrap the App component with QueryClientProvider and pass the queryClient as a prop
    - Remove unused imports and update StravaProvider import

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import StravaProvider from './helpers/context';
+import { StravaProvider } from './helpers/StravaContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
+const queryClient = new QueryClient();
+
 root.render(
-  <StravaProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StravaProvider>,
+  <QueryClientProvider client={queryClient}>
+    <StravaProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StravaProvider>
+  </QueryClientProvider>,
 );
